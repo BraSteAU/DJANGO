7fe3727915be45121a76a854a15ecfe8fa8b7252
BraSteAU <brayan_8924@hotmail.com>
2025-05-16 20:35:05 -0500

    Actualizacion

--- relatos/urls.py
+++ relatos/urls.py
@@ -4,6 +4,9 @@ from . import views
 
 urlpatterns = [
     path('',views.home,name='home'),
-    path('',views.acerca,name='acerca'),
-    path('relatos/',views.lista_relatos,name='lista_relatos')
+    path('acerca',views.acerca,name='acerca'),
+    path('relatos/',views.lista_relatos,name='lista_relatos'),
+    path('autores/',views.lista_autores,name='lista_autores'),
+    path('autores/agregar/',views.agregar_autor,name='agregar_autor'),
+    path('relatos/agregar/',views.agregar_relatos,name='agregar_relatos'),
 ]
--- relatos/views.py
+++ relatos/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Relato
+from django.shortcuts import render,redirect
+from .models import Relato,Autor
+from .forms import AutorForm,RelatoForm
+
 
 # Create your views here.
 
@@ -26,3 +28,29 @@ def lista_relatos(request):
     }
 
     return render(request,'relatos/lista_relatos.html',contexto)
+
+def agregar_autor(request):
+    #Vista para mostrar y procesar el formulario de creacion de autores
+    if request.method == 'POST':
+        form = AutorForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_autores')
+    else:
+        form = AutorForm
+    return render(request,'relatos/agregar_autor.html',{'form': form, 'titulo':'Agregar Autor'})
+
+def agregar_relatos(request):
+    if request.method == 'POST':
+        form = RelatoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('lista_relatos')
+    else:
+        form = RelatoForm
+    return render(request,'relatos/agregar_relatos.html',{'form': form,'titulo':'Agregar Relato'})
+
+def lista_autores(request):
+    autores = Autor.objects.all()
+    return render(request,'relatos/lista_autores.html',{'autores': autores,'titulo': 'Listado de Autores'})
+        
