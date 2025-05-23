fd44458068db0fad12958799749ceeaf10f99bbd
BraSteAU <brayan_8924@hotmail.com>
2025-05-23 18:46:57 -0500

    Update urls.py

--- relatos/urls.py
+++ relatos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+<<<<<<< HEAD
     path('', views.home, name='home'),
     path('acerca/', views.acerca, name='acerca'),
     path('relatos/', views.lista_relatos, name='lista_relatos'),
@@ -10,3 +11,12 @@ urlpatterns = [
     path('autores/', views.lista_autores, name='lista_autores'),
     path('autores/agregar/', views.agregar_autor, name='agregar_autor' ),
 ]
+=======
+    path('',views.home,name='home'),
+    path('acerca',views.acerca,name='acerca'),
+    path('relatos/',views.lista_relatos,name='lista_relatos'),
+    path('autores/',views.lista_autores,name='lista_autores'),
+    path('autores/agregar/',views.agregar_autor,name='agregar_autor'),
+    path('relatos/agregar/',views.agregar_relatos,name='agregar_relato'),
+]
+>>>>>>> 345886e3a7d9ea7681bba861300538be2f95f471
